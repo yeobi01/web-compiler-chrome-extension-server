e720e4d6eb20668d93821c1a3b764cc8dba0868c
yeobi01 <yeobi_01@naver.com>
2025-03-27 16:21:36 +0900

    feat: compile controller, service 구현

--- src/main/java/com/yeobi/compiler/controller/CompileController.java
+++ src/main/java/com/yeobi/compiler/controller/CompileController.java
@@ -1,6 +1,7 @@
 package com.yeobi.compiler.controller;
 
 import com.yeobi.compiler.dto.CompileDTO;
+import com.yeobi.compiler.service.CompileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/compile")
 public class CompileController {
 
+    private final CompileService compileService;
+
     @PostMapping
     public ResponseEntity<CompileDTO.CompileResponse> compile(
             @RequestBody CompileDTO.CompileRequest compileRequest
-    ) {
+    ) throws IOException, InterruptedException {
+        CompileDTO.CompileResponse compileResponse = compileService.compile(compileRequest);
 
-
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+        return ResponseEntity.status(HttpStatus.CREATED).body(compileResponse);
     }
 }
